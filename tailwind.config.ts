5b1e37be2c35734bf6d419d0822bfa76f21bce8d
dev-rome <84602714+dev-rome@users.noreply.github.com>
2024-06-22 21:01:59 -0400

    added primary and secondary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,16 @@ const config: Config = {
         dmSerifDisplay: ["var(--font-dm-serif-display)"],
         publicSans: ["var(--font-public-sans)"],
       },
+      colors: {
+        // primary colors
+        "dark-pink": "#ba4270",
+        "water-white": "#fbfcfe",
+        // secondary colors
+        "san-juan-blue": "#36536b",
+        "mirage-blue": "#1b262f",
+        "charm-pink": "#da6d97",
+        "light-san-juan-blue": "#6c8294",
+      },
     },
   },
   plugins: [],
